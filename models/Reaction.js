cf693f2896c757c71e6b2080232549e7c4f7fa2f
kylemoely <kylemoely@gmail.com>
2023-06-04 19:12:42 -0500

    Formulated reaction and thought models

---
+++ models/Reaction.js
@@ -0,0 +1,17 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: { type: Schema.Types.ObjectId, default: () => new Types.ObjectId() },
+        reactionBody: { type: String, required: true, max_length: 280 },
+        username: { type: String, required: true },
+        createdAt: { type: Date, default: Date.now, get: today => new Date(today).toLocaleDateString() }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+module.exports = reactionSchema;
